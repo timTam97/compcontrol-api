d4bf801306baee26d6c3b0593630209bf6a72626
Timothy Samraj <tsam0005@student.monash.edu>
2020-12-01 17:41:28 +1100

    Remove debug code

--- onconnect/app.py
+++ onconnect/app.py
@@ -11,6 +11,5 @@ table = boto3.resource("dynamodb").Table(os.environ.get("TABLE_NAME"))
 
 
 def handler(event, context):
-    print(os.environ)
     table.put_item(Item={"connectionId": event["requestContext"]["connectionId"]})
     return {"isBase64Encoded": False, "statusCode": 200, "body": "yes"}
