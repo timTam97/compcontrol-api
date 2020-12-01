ae668d8fabd191c6ea7007d603ed5b3539f3cc03
Timothy Samraj <tsam0005@student.monash.edu>
2020-12-01 14:02:36 +1100

    Add validation when sending commands to websocket clients

--- sendcommand/app.py
+++ sendcommand/app.py
@@ -3,7 +3,6 @@ import os
 
 import boto3
 
-sns = boto3.client("sns")
 table = boto3.resource("dynamodb").Table(os.environ.get("TABLE_NAME"))
 # https://github.com/boto/boto3/issues/1914
 apigw = boto3.client(
@@ -12,11 +11,22 @@ apigw = boto3.client(
 
 
 def handler(event, context):
+    command = event["pathParameters"]["command"]
+    allowed_commands = list(json.loads(os.environ.get("ALLOWED_COMMANDS")).values())
+    if command not in allowed_commands:
+        return {
+            "isBase64Encoded": False,
+            "statusCode": 403,
+            "body": json.dumps({"message": "Command not whitelisted"}),
+        }
     base_url = os.environ.get("CONNECTION_BASE_URL")
     res = table.scan(Select="ALL_ATTRIBUTES")
     connIDs = [dict["connectionId"] for dict in res["Items"]]
     for val in connIDs:  # Send to all connections
-        apigw.post_to_connection(
-            Data=event["pathParameters"]["command"], ConnectionId=val
-        )
-    return {"isBase64Encoded": False, "statusCode": 200, "body": "yes"}
+        payload = json.dumps({"type": "command", "sub": command})
+        apigw.post_to_connection(Data=payload, ConnectionId=val)
+    return {
+        "isBase64Encoded": False,
+        "statusCode": 200,
+        "body": json.dumps({"message": "Success"}),
+    }
