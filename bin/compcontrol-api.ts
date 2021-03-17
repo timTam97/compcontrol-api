f189f29e107932433bbe507ed0f39114af3bed0c
Timothy Samraj <tsam0005@student.monash.edu>
2021-03-17 11:51:50 +1100

    Formatting

--- bin/compcontrol-api.ts
+++ bin/compcontrol-api.ts
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
-import 'source-map-support/register';
-import * as cdk from '@aws-cdk/core';
-import { CompControlApiStack } from '../lib/infrastructure/compcontrol-api-stack';
+import "source-map-support/register";
+import * as cdk from "@aws-cdk/core";
+import { CompControlApiStack } from "../lib/infrastructure/compcontrol-api-stack";
 
 const app = new cdk.App();
-new CompControlApiStack(app, 'CompcontrolApiStack');
+new CompControlApiStack(app, "CompcontrolApiStack");
